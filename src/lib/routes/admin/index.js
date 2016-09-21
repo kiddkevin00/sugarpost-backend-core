05ad909ad292b4fbd2b016593ad10daa32870c92
Marcus Hsu <Marcus.Hsu@adp.com>
2016-09-21 18:20:08 -0400

    Fix a unit test

--- src/lib/routes/admin/index.js
+++ src/lib/routes/admin/index.js
@@ -1,8 +1,8 @@
-const { sampleController } = require('../../controllers/');
+const { SampleController } = require('../../controllers/');
 const { Router } = require('express');
 
 const router = Router();
 
-router.get('/sample', sampleController.sampleReq);
+router.get('/sample', SampleController.sampleReq);
 
 module.exports = exports = router;
