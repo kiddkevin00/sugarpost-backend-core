33ed7e039f08519bf491cca72895ee06b1111e55
Marcus Hsu <Marcus.Hsu@adp.com>
2016-11-10 11:36:51 -0500

    Refactor codes

--- src/spec/unit/storage/store/base.spec.js
+++ src/spec/unit/storage/store/base.spec.js
@@ -3,15 +3,15 @@ const Store = require('../../../../lib/storage/store/base');
 describe('High-level (base) store', () => {
 
   it('implements upsert functionality', () => {
-    expect(Store).to.have.property('upsert').that.is.an('function');
+    expect(Store).to.have.property('upsert').that.is.a('function');
   });
 
   it('implements resetting table functionality', () => {
-    expect(Store).to.have.property('resetTable').that.is.an('function');
+    expect(Store).to.have.property('resetTable').that.is.a('function');
   });
 
   it('implements resetting DB functionality', () => {
-    expect(Store).to.have.property('resetDb').that.is.an('function');
+    expect(Store).to.have.property('resetDb').that.is.a('function');
   });
 
 });
