b651fa91a7c4a9493e21da3a2bafb2408624ed7d
Marcus Hsu <Marcus.Hsu@adp.com>
2016-11-15 18:44:26 -0500

    Refactor codes

--- src/lib/services/sample-service.js
+++ src/lib/services/sample-service.js
@@ -1,8 +1,25 @@
 class SampleService {
 
   static execute(state, strategy) {
+    
+    const result = {
+      data: {
+        teachers: [
+          {
+            _id: '001',
+            name: 'Chris',
+            bankAccountId: '002',
+          },
+          {
+            _id: '002',
+            name: 'Mike',
+            bankAccountId: '003',
+          },
+        ],
+      },
+    };
 
-    return Promise.resolve();
+    return Promise.resolve(result);
   }
 
 }
