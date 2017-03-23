f9343fbfb4c21c5cb4e61e9606a618c226debf84
Marcus Hsu <kiddkevin01@gmail.com>
2017-03-23 17:15:14 -0400

    Remove subscription to Signup list in MailChimp once paid

--- src/lib/controllers/payment.controller.js
+++ src/lib/controllers/payment.controller.js
@@ -7,6 +7,7 @@ const constants = require('../constants/');
 const stripeApi = require('stripe');
 const Mailchimp = require('mailchimp-api-v3');
 const couponCode = require('coupon-code');
+const md5 = require('blueimp-md5');
 const jwt = require('jsonwebtoken');
 const mongojs = require('mongojs');
 const Promise = require('bluebird');
@@ -17,7 +18,8 @@ const stripeQuantity = constants.CREDENTIAL.STRIPE.QUANTITY;
 const stripeRecurringBillingDate = constants.CREDENTIAL.STRIPE.RECURRING_BILLING_DATE;
 const stripeReferralCredit = constants.CREDENTIAL.STRIPE.REFERRAL_CREDIT;
 const mailchimp = new Mailchimp(constants.CREDENTIAL.MAIL_CHIMP.API_KEY);
-const mailChimpListId = constants.CREDENTIAL.MAIL_CHIMP.SUBSCRIBED_LIST_ID;
+const mailChimpSubscribedListId = constants.CREDENTIAL.MAIL_CHIMP.SUBSCRIBED_LIST_ID;
+const mailChimpSignupListId = constants.CREDENTIAL.MAIL_CHIMP.SIGNUP_LIST_ID;
 const jwtSecret = constants.CREDENTIAL.JWT.SECRET;
 const jwtAudience = constants.CREDENTIAL.JWT.AUDIENCE;
 const jwtIssuer = constants.CREDENTIAL.JWT.ISSUER;
@@ -177,8 +179,8 @@ class PaymentController {
         return stripe.subscriptions.update(stripeSubscriptionId, { trial_end, prorate });
       })
       .then(() => mailchimp.post({
-        path: '/lists/{mailChimpListId}/members/',
-        path_params: { mailChimpListId },
+        path: '/lists/{mailChimpSubscribedListId}/members/',
+        path_params: { mailChimpSubscribedListId },
         body: {
           email_address: state.email,
           status: 'subscribed',
@@ -245,6 +247,31 @@ class PaymentController {
                 }));
             });
         }
+
+        mailchimp
+          .delete({
+            path: '/lists/{mailChimpSignupListId}/members/{hashedEmail}',
+            path_params: {
+              mailChimpSignupListId,
+              hashedEmail: md5(state.email),
+            },
+          })
+          .catch((_err) => {
+            const err = new StandardErrorWrapper(_err);
+
+            err.append({
+              code: constants.SYSTEM.ERROR_CODES.INTERNAL_SERVER_ERROR,
+              name: constants.SYSTEM.ERROR_NAMES.CAUGHT_ERROR_IN_PAYMENT_CONTROLLER,
+              source: constants.SYSTEM.COMMON.CURRENT_SOURCE,
+              message: constants.SYSTEM.ERROR_MSG.CAUGHT_ERROR_IN_PAYMENT_CONTROLLER,
+            });
+
+            return res.status(constants.SYSTEM.HTTP_STATUS_CODES.BAD_REQUEST)
+              .json(err.format({
+                containerId: state.context.containerId,
+                requestCount: state.context.requestCount,
+              }));
+          });
 
         const newJwtPayload = Object.assign({}, req.user, partialNewUserInfo, {
           sub: `${partialNewUserInfo.type}:${req.user.email}:${req.user._id}`,
