52218a8d15fd31678293a6f75530bfd7b8874811
Marcus Hsu <kiddkevin01@gmail.com>
2017-03-31 20:10:00 -0400

    Switch back to Stripe test mode

--- src/lib/constants/credential/production.js
+++ src/lib/constants/credential/production.js
@@ -7,8 +7,8 @@ exports.JWT = {
 };
 
 exports.STRIPE = {
-  PRIVATE_KEY: 'sk_live_ypetQeGEadd6Qq5L6QIGkUn1',
-  PLAN_ID: 'test-01',
+  PRIVATE_KEY: 'sk_test_ccdvoeJH9W86JXhx85PEgkvi',
+  PLAN_ID: '4-desserts-per-month',
   QUANTITY: 1,
   RECURRING_BILLING_DATE: 25,
   REFERRAL_CREDIT: 272,
